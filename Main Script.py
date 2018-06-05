1d2f64fedd204416c27d9e1db66497ae7fa1bf74
Akash <akash.darknight@gmail.com>
2018-06-05 15:05:00 +0530

    Deep Convolutional GANs

---
+++ Main Script.py
@@ -0,0 +1,7 @@
+# Build a deep convolutional generative adversarial network (DCGAN) to generate digit images from a noise distribution with TensorFlow.
+
+from __future__ import division, print_function, absolute_import
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
