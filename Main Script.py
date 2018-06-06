5606adf33c3e7b45fac54f631018a2830747a9da
Akash <akash.darknight@gmail.com>
2018-06-06 15:05:00 +0530

    Deep Convolutional GANs

--- Main Script.py
+++ Main Script.py
@@ -5,3 +5,8 @@ from __future__ import division, print_function, absolute_import
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
+
+
+# Import MNIST data
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
