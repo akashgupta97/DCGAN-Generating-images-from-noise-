4ca1a67050bea8721d4496a78198165d0d477111
Akash <akash.darknight@gmail.com>
2018-06-08 16:37:00 +0530

    Deep Convolutional GANs

--- Main Script.py
+++ Main Script.py
@@ -10,3 +10,14 @@ import tensorflow as tf
 # Import MNIST data
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+
+# Training Params
+num_steps = 10000
+batch_size = 128
+lr_generator = 0.002
+lr_discriminator = 0.002
+
+# Network Params
+image_dim = 784 # 28*28 pixels * 1 channel
+noise_dim = 100 # Noise data points
